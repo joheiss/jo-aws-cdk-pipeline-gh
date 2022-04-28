15f31d0dd4650861f82d5c9d317766801cc4767f
Josef Heiss <josef.heiss@jovisco.de>
2022-04-28 12:13:40 +0200

    added billing stack

--- bin/jo-aws-cdk-pipeline-gh.ts
+++ bin/jo-aws-cdk-pipeline-gh.ts
@@ -3,10 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { ServiceStack } from '../lib/service-stack';
+import { BillingStack } from '../lib/billing-stack';
 
 const app = new cdk.App();
 
 const pipelineStack = new PipelineStack(app, 'PipelineStack', {});
 const serviceStack = new ServiceStack(app, 'ServiceStack');
+const billingStack = new BillingStack(app, 'BillingStack');
 
-pipelineStack.addDeployStage(serviceStack, 'Service_Deploy');
+const deplayStage = pipelineStack.addDeployStage(serviceStack, 'Service_Deploy');
+pipelineStack.addBillingStackToDeployStage(billingStack, deplayStage);
