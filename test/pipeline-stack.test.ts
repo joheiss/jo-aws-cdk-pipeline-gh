26757c3b90dbd3101f04f2ff43763ac03bdba487
Josef Heiss <josef.heiss@jovisco.de>
2022-04-28 11:30:36 +0200

    more tests

--- test/pipeline-stack.test.ts
+++ test/pipeline-stack.test.ts
@@ -1,13 +1,14 @@
 import { App, Stack } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 import { PipelineStack } from '../lib/pipeline-stack';
+import { ServiceStack } from '../lib/service-stack';
 
 describe('Pipeline Stack', () => {
 
     let app: App;
     let stack: PipelineStack;
     
-    beforeAll(() => {
+    beforeEach(() => {
         app = new App();
         stack = new PipelineStack(app, 'PipelineStack');
     });
@@ -15,5 +16,18 @@ describe('Pipeline Stack', () => {
     test('Stack', () => {
         const cfn = Template.fromStack(stack).toJSON();
         expect(cfn).toMatchSnapshot();
+    });
+
+    test('Add service stage', () => {
+        // GIVEN
+        const serviceStack = new ServiceStack(app, 'ServiceStack');
+        // WHEN
+        stack.addDeployStage(serviceStack, 'Deploy');
+        // THEN
+        Template.fromStack(stack).hasResourceProperties('AWS::CodePipeline::Pipeline', {
+            Stages: Match.arrayWith([
+                Match.objectLike({ Name: "Deploy" }),
+            ]),
+        });
     });
 });
